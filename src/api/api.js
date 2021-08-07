9df10ae20efead2928c2a620725d9deb32243c22
golamrabbiazad <golamrabbiazad@gmail.com>
2021-08-07 13:53:48 +0600

    routes are handled by express

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,18 @@
-"use strict";
+const express = require("express");
+const serverless = require("serverless-http");
 
-module.exports.handler = async (event, ctx, done) => {
-  done(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "hello",
-    }),
-  });
+const app = express();
+
+app.get("/", function (req, res) {
+  return res.send("Hello World!");
+});
+
+const handler = serverless(app);
+module.exports.handler = async (event, context) => {
+  //github.com/dougmoscrop/serverless-http/issues/86#issuecomment-849958755
+  https: event.path = event.path === "" ? "/" : event.path;
+
+  const result = await handler(event, context);
+
+  return result;
 };
